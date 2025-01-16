84cd525d38b287dc9b525f853de327ef27696973
FranMurcianoRdev <franmurcianor.dev@gmail.com>
2025-01-16 21:38:30 +0100

    layout

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -49,10 +49,10 @@ const FormModal = ({
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
   const bgColor =
     type === "create"
-      ? "bg-lamaYellow"
+      ? "bg-yellow"
       : type === "update"
-      ? "bg-lamaSky"
-      : "bg-lamaPurple";
+      ? "bg-sky"
+      : "bg-purple";
 
   const [open, setOpen] = useState(false);
 
